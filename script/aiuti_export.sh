10df42bfd2c8e2d7763a6026f089931fd548f133
aborruso <aborruso@gmail.com>
2024-12-25 12:57:44 +0100

    corretta logica

--- script/aiuti_export.sh
+++ script/aiuti_export.sh
@@ -10,18 +10,28 @@ folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 mkdir -p "${folder}"/tmp
 mkdir -p "${folder}"/../data/aiuti/parquet
 
-find ../data/aiuti/raw -name "*.zip" | head -n 5 | while read zip_file; do
-  name=$(basename "$zip_file" .zip)
+debug=false
 
+if [ "$debug" = true ]; then
+  find "${folder}"/../data/aiuti/parquet -name "*.parquet" -delete
+fi
+
+find ../data/aiuti/raw -name "*.zip" | while read zip_file; do
+  name=$(basename "$zip_file" .zip)
 
   if [ -f "${folder}"/../data/aiuti/parquet/"${name}"_aiuti.parquet ]; then
     echo "già creato"
   else
     echo "lo creo"
+
+    # estrai file xml
     unzip -p "$zip_file" | sed "s/\x02/ /g;s/\x10/ /g;s/\x1A/'/g" >"${folder}"/tmp/"$name".xml
+
+    # fai il flattening dell'xml in aiuti, componenti e strumenti
     python3 flattening.py -i "${folder}"/tmp/"$name".xml -of "${folder}"/../data/aiuti/parquet
+
+    # elimina file xml
+    find "${folder}"/tmp/ -name "*.xml" -delete
   fi
 
 done
-
-find "${folder}"/tmp/ -name "*.xml" -delete
